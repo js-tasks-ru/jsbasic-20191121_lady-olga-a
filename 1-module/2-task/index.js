75247b8a45b54478c03136ae654ad951afaf83d2
unknown <lady.olga.a@gmail.com>
2019-11-26 23:55:04 +0300

    Задача 2

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,7 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  if ((name.length > 3) && (name.indexOf(' ') === -1 )) {
+  if ((name.length > 3) && (name.indexOf(' ') === -1)) {
     return true;
   } else {
     return false;
