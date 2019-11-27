787a44d7a3d032c76ccc0b5e053b91a7bbb1010e
unknown <lady.olga.a@gmail.com>
2019-11-27 22:46:03 +0300

    Функция pow(x,n)

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,6 +4,17 @@
  * @param {number} n index
  * @returns {number}
  */
+let x = prompt('Число x, которое возводим в степень:', 0);
+let n = prompt('Степень n, в которую возводим (больше 1):', 1);
+if ((n < 1) || (n % 1 != 0) || !isFinite(x)) {
+  alert('основание должно быть число,  степень - целое число больше 1');
+} else {
+  alert(pow(x, n));
+}
 function pow(m, n) {
-
+  let result = m;
+  for (i = 1; i < n; i++) {
+    result = result * m;
+  }
+  return result;
 }
