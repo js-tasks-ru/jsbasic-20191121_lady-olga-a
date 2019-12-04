e9565fcbc8ae1c5c85351c5bf1fbbe244edcfb6d
unknown <lady.olga.a@gmail.com>
2019-12-04 21:06:04 +0300

    Клонирование объекта

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,4 +4,20 @@
  * @returns {Object}
  */
 function clone(obj) {
+  let obb = {};
+  if (obj === null) {
+    return obj;
+  }
+  for (let key in obj) {
+    if (typeof obj[key] === null) {
+      obb[key] = obj[key];
+    } else {
+      if (typeof obj[key] === 'object') {
+        obb[key] = clone(obj[key]);
+      } else {
+        obb[key] = obj[key];
+      }  
+    }
+  }
+  return obb;
 }
