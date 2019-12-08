02b736ae1095d50c4c837e5a6d36d490778750c1
unknown <lady.olga.a@gmail.com>
2019-12-09 01:28:31 +0300

    Модуль 3 задача 2

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,18 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+  let arr = str.split(' ');
+  let arrNew = [];
+  let arr2 = [];
+  for (key in arr) {
+    if (arr[key].indexOf(',') > 0) {
+      arr2 = arr[key].split(',');
+      arrNew = arr.concat(arr2);
+    } else {
+      arrNew.push(arr[key]);
+    }
+  }  
+  let arr1 = arrNew.filter(item => isFinite(item));
+  arr1.sort((a, b) => a - b);
+  return {min: +arr1[0], max: +arr1[arr1.length - 1]};
 }
