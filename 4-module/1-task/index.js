8c7828e09411751c92834fcc509dff7c9d69baa8
unknown <lady.olga.a@gmail.com>
2019-12-11 20:23:08 +0300

    Модуль 4 Список друзей

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,12 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+  let ul = document.createElement('ul');
+  let li = {};
+  for (friend of friends) {
+    li = document.createElement('li');
+    li.innerHTML = `${friend.firstName} ${friend.lastName}`;
+    ul.appendChild(li);
+  }
+  return ul;	 
 }
