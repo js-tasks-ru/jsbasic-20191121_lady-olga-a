c320a0e2ff0f53766354443c98c7ed9ff06d4d40
unknown <lady.olga.a@gmail.com>
2019-12-14 00:14:19 +0300

    Модуль 5 задача 1

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -3,4 +3,25 @@
  * @param {Element} table
  */
 function highlight(table) {
+  for (let i = 1; i < table.rows.length; i++) {
+    const row = table.rows[i];
+    if (row.cells[2].innerHTML === 'f') {
+      row.classList.add('female');
+    }
+    if (row.cells[2].innerHTML === 'm') {
+      row.classList.add('male');
+    }
+    if (row.cells[3].dataset.available === 'true') {
+      row.classList.add('available');
+    }
+    if (row.cells[3].dataset.available === 'false') {
+      row.classList.add('unavailable');
+    } 
+    if (row.cells[3].dataset.available === undefined) {
+      row.hidden = true;
+    } 
+    if (+row.cells[1].innerHTML < 18) {
+      row.style = 'text-decoration: line-through';
+    }  
+  }
 }
